6cc5f1b7a73420bfd38d18fab8c5abcb0394df80
andrea <andrea.gitcommit@gmail.com>
2025-04-18 19:12:36 -0400

    Query to temporarily add new vendor to New Vendor

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -124,7 +124,15 @@ When inserting the new vendor, you need to appropriately align the columns to be
 -> To insert the new row use VALUES, specifying the value you want for each column:
 VALUES(col1,col2,col3,col4,col5) 
 */
+DROP TABLE IF EXISTS temp.new_vendor;
 
+CREATE TEMP TABLE temp.new_vendor AS 
+
+SELECT *
+FROM vendor;
+
+INSERT INTO temp.new_vendor
+VALUES(10, 'Thomass Superfood Store', 'Fresh Focused', 'Thomas', 'Rosenthal')
 
 
 -- Date -- SKIPPED
