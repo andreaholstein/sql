fafaeb555c6cc062268118356906214a057e0223
andrea <andrea.gitcommit@gmail.com>
2025-04-26 19:42:27 -0400

    Working on Query to Cross Join Vendors, Products, and potential Sales

--- 02_activities/assignments/assignment2.sql
+++ 02_activities/assignments/assignment2.sql
@@ -175,41 +175,36 @@ Think a bit about the row counts: how many distinct vendors, product names are t
 How many customers are there (y). 
 Before your final group by you should have the product of those two queries (x*y).  */
 	
--- DROP TABLE IF EXISTS temp.vendor_products;
--- 
--- CREATE TEMP TABLE temp.vendor_products AS
--- SELECT DISTINCT v.vendor_name, p.product_name, (vi.original_price*5) AS five_purchased
--- FROM 
--- (
--- 	SELECT DISTINCT vendor_id, vendor_name 
--- 	FROM vendor
--- ) AS v
--- 
--- INNER JOIN vendor_inventory vi 
--- 	ON v.vendor_id = vi.vendor_id
--- 	
--- INNER JOIN 
--- (
--- 	SELECT DISTINCT product_id, product_name 
--- 	FROM product
--- ) AS p 
--- 	ON p.product_id = vi.product_id
--- 	
--- ORDER BY v.vendor_name;
--- 
--- SELECT vendor_name
+DROP TABLE IF EXISTS temp.vendor_products;
+
+CREATE TEMP TABLE temp.vendor_products AS
+SELECT DISTINCT v.vendor_name
+,p.product_name
+,(vi.original_price*5) AS five_purchased
+FROM vendor v
+
+JOIN vendor_inventory vi 
+	ON v.vendor_id = vi.vendor_id
+	
+JOIN product p
+	ON p.product_id = vi.product_id
+	
+ORDER BY v.vendor_name;
+
+SELECT 
+*
+-- vendor_name
 -- ,product_name
 -- ,five_purchased
--- 
--- FROM vendor_products vp
--- 
--- CROSS JOIN
--- (
--- 	SELECT COUNT(customer_id) AS total_customers
--- 	FROM customer
--- )AS c
--- GROUP BY vp.product_name,
--- ORDER BY vp.vendor_name, vp.product_name;
+FROM vendor_products vp
+
+CROSS JOIN
+(
+	SELECT COUNT(c.customer_id) AS total_customers
+	FROM customer
+)AS c
+GROUP BY vp.product_name,
+ORDER BY vp.vendor_name, vp.product_name;
 
 -- INSERT
 /*1.  Create a new table "product_units". 
@@ -217,27 +212,27 @@ This table will contain only products where the `product_qty_type = 'unit'`.
 It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
 Name the timestamp column `snapshot_timestamp`. */
 
-DROP TABLE IF EXISTS product_units;
-CREATE TABLE product_units AS 
-	SELECT * , datetime() as snapshot_timestamp
-	FROM product
-	WHERE product_qty_type = 'unit' 
-	ORDER BY product_name;
+-- DROP TABLE IF EXISTS product_units;
+-- CREATE TABLE product_units AS 
+-- 	SELECT * , datetime() as snapshot_timestamp
+-- 	FROM product
+-- 	WHERE product_qty_type = 'unit' 
+-- 	ORDER BY product_name;
 
 
 /*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 
 This can be any product you desire (e.g. add another record for Apple Pie). */
 
-INSERT INTO product_units
-VALUES(101, 'Dalmatians', 'puppy', 7, 'unit', datetime());
+-- INSERT INTO product_units
+-- VALUES(101, 'Dalmatians', 'puppy', 7, 'unit', datetime());
 
 -- DELETE
 /* 1. Delete the older record for the whatever product you added. 
 
 HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
 
-DELETE FROM product_units
-WHERE product_id = 101;
+-- DELETE FROM product_units
+-- WHERE product_id = 101;
 
 -- UPDATE
 /* 1.We want to add the current_quantity to the product_units table. 
@@ -256,30 +251,29 @@ Finally, make sure you have a WHERE statement to update the right row,
 	you'll need to use product_units.product_id to refer to the correct row within the product_units table. 
 When you have all of these components, you can run the update statement. */
 
-ALTER TABLE product_units
-ADD current_quantity INT;
-SELECT * FROM product_units; 
-
-DROP TABLE IF EXISTS temp.qty_check;
-
-CREATE TEMP TABLE temp.qty_check AS
-SELECT * 
-FROM(
-	SELECT DISTINCT 
-	product_id
-	,quantity
-	,ROW_NUMBER() OVER(PARTITION BY product_id ORDER BY market_date DESC) as latest_qty
-	,COALESCE(quantity, 0) AS zeroed_qty
-	FROM vendor_inventory	
-)
-WHERE latest_qty = 1;
-
-UPDATE product_units
-SET current_quantity = temp.qty_check.zeroed_qty
-FROM temp.qty_check
-WHERE product_units.product_id = temp.qty_check.product_id;
-
-SELECT product_id, product_name, current_quantity 
-FROM product_units
-ORDER BY product_id;
-
+-- ALTER TABLE product_units
+-- ADD current_quantity INT;
+-- SELECT * FROM product_units;
+-- 
+-- DROP TABLE IF EXISTS temp.qty_check;
+-- 
+-- CREATE TEMP TABLE temp.qty_check AS
+-- SELECT * 
+-- FROM(
+-- 	SELECT DISTINCT 
+-- 	product_id
+-- 	,quantity
+-- 	,ROW_NUMBER() OVER(PARTITION BY product_id ORDER BY market_date DESC) as latest_qty
+-- 	,COALESCE(quantity, 0) AS zeroed_qty
+-- 	FROM vendor_inventory	
+-- )
+-- WHERE latest_qty = 1;
+-- 
+-- UPDATE product_units
+-- SET current_quantity = temp.qty_check.zeroed_qty
+-- FROM temp.qty_check
+-- WHERE product_units.product_id = temp.qty_check.product_id;
+-- 
+-- SELECT product_id, product_name, current_quantity 
+-- FROM product_units
+-- ORDER BY product_id;
